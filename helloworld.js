50354ee727dfc63f4b2232f70aad16f928cd176f
Kasulejoseph <kasule06joseph@gmail.com>
2023-04-14 09:14:09 +0300

    CFG course

---
+++ helloworld.js
@@ -0,0 +1,12 @@
+
+function sayHello() {
+    return "Hello World"
+}
+
+function sayBye () {
+    const name = "Joseph"
+    return `Good Bye ` + name
+}
+
+console.log(sayHello())
+console.log(sayBye())
